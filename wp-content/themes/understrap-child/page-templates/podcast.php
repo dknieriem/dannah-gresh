a5e1de02d1dd79fbf2f2155ecd07db6b0cb50719
Pete Detweiler <peter.detweiler@gmail.com>
2019-01-27 15:09:32 -0500

    Podcast Updates

--- wp-content/themes/understrap-child/page-templates/podcast.php
+++ wp-content/themes/understrap-child/page-templates/podcast.php
@@ -25,7 +25,6 @@ $podcastSlug = types_render_field( "page-podcast-slug", array( "output" => "raw"
                         <img class="podcast-intro__post-image" src="<?php echo $featuredImage;?>" alt="Featured image" />
                     </div>
                     <div class="podcast-intro__post-meta">
-                        <p class="podcast-intro__subtitle">Podcast</p>
                         <h1 class="podcast-intro__title"><?php the_title(); ?></h1>
                         <div class="podcast-intro__description-wrapper">
                             <p class="podcast-intro__description"><?php echo  types_render_field( "podcast-description", array( "output" => "raw"));?></p>
@@ -56,25 +55,36 @@ $podcastSlug = types_render_field( "page-podcast-slug", array( "output" => "raw"
                         'post_type'        => $podcastSlug,
                         'posts_per_page'   => 25,
                         'orderby' => 'post_date',
-                        'order' => 'ASC'
+                        'order' => 'DESC'
                         );
                         $query = new WP_Query( $args );
                     ?>
                     <?php while ( $query->have_posts() ) : the_post(); ?>
                         <?php $query->the_post(); ?>
-                        <div class="podcast-summary">
-                            <div class="podcast-summary__meta">
-                                <div class="podcast-summary__date">
-                                    <?php echo get_the_date(); ?>
+                        <div class="podcast-row">
+                            <div class="podcast-row__summary collapsed" data-toggle="collapse" data-target="#details-<?php echo get_the_id(); ?>">
+                                <div class="podcast-row__date">
+                                    <?php echo get_the_date('M j, Y'); ?>
                                 </div>
-                                <h2 class="podcast-summary__title"><?php the_title(); ?></h2>
-                                <p class="podcast-summary__description"><?php the_content(); ?></p>
-                            </div>  
-                        </div>                      
-                        
+                                <h2 class="podcast-row__title"><?php the_title(); ?></h2>
+                                <img class="podcast-row__collapse-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/img/collapse-icon.png" alt="">
+                            </div>
+                            <div class="podcast-row__meta">
+                                <?php $postId = get_the_id(); ?>
+                                <div id="details-<?php echo $postId ?>" class="podcast-row__details collapse">
+                                    <p class="podcast-row__description"><?php the_content(); ?></p>
+
+                                    <?php $resources = get_post_meta( $postId, 'wpcf-related-resources', true );?>
+                                    <?php if ($resources) { ?>
+                                        <h3 class="podcast-row__resources-header">More resources</h3>
+                                    <?php } ?>
+                                    <div class="podcast-row__resources"><?php echo $resources; ?></div>
+                                </div>
+                            </div>
+                        </div>                         
                     <?php endwhile; // end of the loop. ?>
                 </div>
-                <div class="page-section__one-third-column" style="margin-top:6rem;">
+                <div class="page-section__one-third-column">
                     <div class="sidebar-cta-card">
                         <h2 class="sidebar-cta-card__heading">Want Dannah to speak at your event?</h2>
                         <p class="sidebar-cta-card__text">Dannah delivers meaty biblical truth in a transparent, conversational style that brings her audience into the presentation. Your audience will laugh. They may cry. They will love the research-based, user-friendly information on popular topics such as biblical womanhood, modesty, purity, raising children to reflect biblical values, the power of prayer, intimacy with God, emotional and sexual healing, and parent-child communication.</p>
